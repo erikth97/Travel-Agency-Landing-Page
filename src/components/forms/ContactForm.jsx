8c8d4f27a7597f18c2b4e490c99b12ef551c4fe9
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-04-24 20:42:09 -0600

    Landing Project FreeLance

--- src/components/forms/ContactForm.jsx
+++ src/components/forms/ContactForm.jsx
@@ -5,34 +5,34 @@ const ContactForm = () => {
   return (
     <div className="flex flex-col sm:flex-row p-5 sm:p-10 m-5 gap-10 sm:m-10 justify-between  flex-wrap">
       <div className="mb-10 sm:mb-0 justify-center flex flex-col ">
-        <InfoContainer title="Phone" description="+330 120 6412 9916" />
-        <InfoContainer title="Email" description="naturenest@example.com" />
+        <InfoContainer title="Telefono" description="Numero Telefonicoooooo" />
+        <InfoContainer title="Correo Electronico" description="naturenest@example.com" />
         <InfoContainer
-          title="Address"
+          title="Direccion"
           description="123 Main Street, Anytown, CA 12345"
         />
       </div>
       <form className="flex flex-col gap-5 justify-start text-black w-full sm:w-auto">
         <div className="flex flex-col sm:flex-row gap-5">
           <input
-            placeholder="Your Name"
+            placeholder="Nombre de la Empresa"
             className="text-black placeholder-gray-500 text-lg bg-transparent border-b border-black outline-none focus:border-none"
           />
           <input
-            placeholder="Your Email"
+            placeholder="Correo Electronico"
             className="text-black placeholder-gray-500 text-lg bg-transparent border-b border-black outline-none focus:border-none"
           />
         </div>
         <input
-          placeholder="Subject"
+          placeholder="Asunto"
           className="text-black placeholder-gray-500 text-lg bg-transparent border-b border-black outline-none focus:border-none"
         />
         <textarea
-          placeholder="Message"
+          placeholder="Mensaje"
           className="text-black placeholder-gray-500 text-lg bg-transparent border-b border-black outline-none focus:border-none h-32"
         />
         <Button
-          text="Submit"
+          text="Enviar"
           style={{ hover: { color: "red" } }}
           className="w-full sm:w-auto"
         />
